fad9acd7856b452d303fe926d5bd90907ce93319
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-16 13:03:09 +0530

    decrement counter test

--- normalapp_unit_test/test/counter_test.dart
+++ normalapp_unit_test/test/counter_test.dart
@@ -6,29 +6,39 @@ void main() {
 
   group('Counter class -', () {
     test(
-      'given counter class when it is instanted then value of count should be 0',
-      () {
-    // Arrange
-    final Counter counter = Counter();
-    // Act
-    final val = counter.count;
-    // Assert
-    expect(val, 0);
-  });
+        'given counter class when it is instanted then value of count should be 0',
+        () {
+      // Arrange
+      final Counter counter = Counter();
+      // Act
+      final val = counter.count;
+      // Assert
+      expect(val, 0);
+    });
 
-  test(
-      'given counter class when it is incremented the value of count should be 1',
-      () {
-    // Arrange
-    final Counter counter = Counter();
+    test(
+        'given counter class when it is incremented the value of count should be 1',
+        () {
+      // Arrange
+      final Counter counter = Counter();
 
-    // Act
-    counter.incrementCounter();
-    final val = counter.count;
+      // Act
+      counter.incrementCounter();
+      final val = counter.count;
 
-    // Assert
-    expect(val, 1);
+      // Assert
+      expect(val, 1);
+    });
+
+    test('given counter class it is decremented then value is -1', () {
+      // Arrange
+      final Counter counter = Counter();
+      // Act
+      counter.decrementCounter();
+      final val = counter.count;
+
+      // Assert
+      expect(val, -1);
+    });
   });
-  });
-  
 }
